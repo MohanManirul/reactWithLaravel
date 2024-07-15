dd1538c7d604eee6197bdebb4d9ad84a0142be03
MohanManirul <fiforeg@gmail.com>
2024-07-16 01:21:25 +0600

    frontend login page using react and laravel

--- resources/js/components/Main.jsx
+++ resources/js/components/Main.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
 import { BrowserRouter , Routes , Route, Link  } from 'react-router-dom';
-import Home from './Home';
-import About from './About';
-import Service from './Service';
-import Login from './Login';
+import Login from './auth/Login';
 
 
 function MyApp(){
     return(
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={ <Home />} />
-                <Route path='/about' element={ <About />}/>
-                <Route path='/service/domain' element={ <Service />}/>
-                <Route path='/login' element={ <Login />}/>
+                <Route path='/' element={ <Login />} />               
             </Routes>
         </BrowserRouter>
     );
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="en" data-layout="twocolumn" data-sidebar="light" data-sidebar-size="lg" data-sidebar-image="none">
+
 <head>
-    <meta charset="UTF-8">
+
+    <meta charset="utf-8" />
+    <title>Sign Up | Velzon - Admin & Dashboard Template</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesbrand" name="author" />
+    <link rel="shortcut icon" href="assets/images/favicon.ico">
+    <script src="{{ asset('assets/js/layout.js') }}"></script>
+    <link href="{{ asset('assets/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/icons.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/app.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset('assets/css/custom.min.css') }}" rel="stylesheet" type="text/css" />
+
+
 </head>
+
 <body>
     <div id="app"></div>
-    <!-- react support -->
+
+    <script src="{{asset('assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{ asset('assets/libs/simplebar/simplebar.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/node-waves/waves.min.js') }}"></script>
+    <script src="{{ asset('assets/libs/feather-icons/feather.min.js') }}"></script>
+    <script src="{{ asset('assets/js/pages/plugins/lord-icon-2.1.0.js') }}"></script>
+    <script src="{{ asset('assets/js/plugins.js') }}"></script>
+    <script src="{{ asset('assets/js/pages/form-validation.init.js') }}"></script>
+    <script src="{{ asset('assets/js/pages/passowrd-create.init.js') }}"></script>
 
     @viteReactRefresh
     @vite('resources/js/app.js')
 
-    <!-- react support -->
 </body>
+
 </html>
